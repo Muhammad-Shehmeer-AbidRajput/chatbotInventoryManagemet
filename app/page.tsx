d3f7977e304eac5976cf30aa032b11e7f1703970
ShehmeerAbidRajput <115403192+ShehmeerAbidRajput@users.noreply.github.com>
2023-09-18 13:32:08 +0500

    check if flask is running

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -38,6 +39,9 @@ export default function Home() {
         />
       </div>
 
+      <Link href="/api/python">
+            <code className="font-mono font-bold">api/index.py</code>
+          </Link>
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="/dashboard"
